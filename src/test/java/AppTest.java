6c8ce7ffc23c211f7e43724cef809046d38f01fe
Lilo Onwuzu <lpr422@gmail.com>
2016-05-16 15:58:55 -0700

    Updated sql files. Added AppTest.java

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -31,12 +31,81 @@ public class AppTest extends FluentTest {
   @ClassRule
   public static ServerRule server = new ServerRule();
 
-  // @Test
-  // // test that home page UI works
-  // public void homePageTest_homePage() {
-  //   goTo("http://localhost:4567/");
-  //   assertThat(pageSource()).contains("please enter your name");
-  // }
+  @Test
+  public void homePageTest_homePage() {
+    goTo("http://localhost:4567/stylists");
+    assertThat(pageSource()).contains("Directory of Stylists and Clients");
+  }
 
+  @Test
+  public void addsANewStylist() {
+    goTo("http://localhost:4567/stylists");
+    click("a", withText("Add New Stylist"));
+    fill("#inputStylist").with("Jane Doe");
+    submit(".btn");
+    assertThat(pageSource()).contains("Jane Doe");
+  }
+
+  @Test
+  public void fullListOfStylist() {
+    goTo("http://localhost:4567/stylists");
+    click("a", withText("Add New Stylist"));
+    fill("#inputStylist").with("Jane Doe");
+    submit(".btn");
+    assertThat(pageSource()).contains("Jane Doe");
+    assertThat(pageSource()).contains("Click on the Stylists below");
+  }
+
+  @Test
+  public void stylist_showsStylistDetails() {
+    goTo("http://localhost:4567/stylists");
+    click("a", withText("Add New Stylist"));
+    fill("#inputStylist").with("Jane Doe");
+    submit(".btn");
+    assertThat(pageSource()).contains("Jane Doe");
+    click("a", withText("Jane Doe"));
+    assertThat(pageSource()).contains("Jane Doe has 0 clients");
+    click("a", withText("Add New Client For Jane Doe"));
+    fill("#inputClient").with("John Doe");
+    submit(".btn");
+    assertThat(pageSource()).contains("John Doe");
+  }
+
+  @Test
+  public void stylist_showsClientDetails() {
+    goTo("http://localhost:4567/stylists");
+    click("a", withText("Add New Stylist"));
+    fill("#inputStylist").with("Jane Doe");
+    submit(".btn");
+    assertThat(pageSource()).contains("Jane Doe");
+    click("a", withText("Jane Doe"));
+    assertThat(pageSource()).contains("Jane Doe has 0 clients");
+    click("a", withText("Add New Client For Jane Doe"));
+    fill("#inputClient").with("John Doe");
+    submit(".btn");
+    assertThat(pageSource()).contains("John Doe");
+    click("a", withText("John Doe"));
+    assertThat(pageSource()).contains("Delete this client");
+    click("a", withText("Delete this client"));
+  }
+
+  @Test
+  public void edit_editsClientDetails() {
+    goTo("http://localhost:4567/stylists");
+    click("a", withText("Add New Stylist"));
+    fill("#inputStylist").with("Jane Doe");
+    submit(".btn");
+    assertThat(pageSource()).contains("Jane Doe");
+    click("a", withText("Jane Doe"));
+    assertThat(pageSource()).contains("Jane Doe has 0 clients");
+    click("a", withText("Add New Client For Jane Doe"));
+    fill("#inputClient").with("John Doe");
+    submit(".btn");
+    assertThat(pageSource()).contains("John Doe");
+    click("a", withText("John Doe"));
+    assertThat(pageSource()).contains("Delete this client");
+    click("a", withText("Delete this client"));
+    assertThat(pageSource()).contains("No clients have been added for Jane Doe!");
+  }
 
 }
